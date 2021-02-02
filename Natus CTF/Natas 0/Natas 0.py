0adbc1424f01c599ccc3a9305daf1e9f941fb09c
Oliver-Francis1997 <78413521+Oliver-Francis1997@users.noreply.github.com>
2021-02-02 13:39:51 +0100

    Update Natas 0.py
    updated

--- Natus CTF/Natas 0/Natas 0.py
+++ Natus CTF/Natas 0/Natas 0.py
@@ -4,5 +4,4 @@ user = 'natas0'
 
 url = 'http://{}.natas.labs.overthewire.org'.format(user)
 r = requests.get(url, auth=(user, user)).text
-#print(re.findall('<!--The password for natas1 is (.*) -->', r)[0])
-print(r)
+print(re.findall('<!--The password for natas1 is (.*) -->', r)[0])
